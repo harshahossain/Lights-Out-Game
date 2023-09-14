ef953bda84bce93eb122fd244d9b7bc499dce122
Harsha Hossain <harshahossain@gmail.com>
2023-09-15 03:56:14 +0600

    Made the app functional added css
    Made the actual logic for functioning this game. Also, Added styling with CSS.

--- src/Board.js
+++ src/Board.js
@@ -32,11 +32,19 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+static defaultProps={
+  nrows:5,
+  ncols:5,
+  chanceLightStartsOn:0.25 
+};
   constructor(props) {
     super(props);
-
+    
     // TODO: set initial state
+    this.state={
+      hasWon:false,
+      board:this.createBoard()
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -44,14 +52,23 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+     for(let y=0;y<this.props.nrows;y++){
+      let row=[];
+      for(let x=0; x<this.props.ncols;x++){
+          row.push(Math.random()<this.props.chanceLightStartsOn)
+      }
+      board.push(row)
+     }
     return board
   }
 
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
+    console.log('flipping')
     let {ncols, nrows} = this.props;
     let board = this.state.board;
+    
     let [y, x] = coord.split("-").map(Number);
 
 
@@ -62,48 +79,41 @@ class Board extends Component {
         board[y][x] = !board[y][x];
       }
     }
-
-    // TODO: flip this cell and the cells around it
-
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
-
-    //this.setState({board, hasWon});
+    flipCell(y,x);//updating the clicked cell
+    //updating ne-ighbourr cell;down below
+    flipCell(y,x-1);//flip left
+    flipCell(y,x+1);//flip right
+    flipCell(y-1,x);//flip down
+    flipCell(y+1,x);//flip up
+    // let hasWon= this.state.hasWon;
+    let hasWon=board.every(row=>row.every(cell=>!cell))//just means every rows every cell should be false
+    this.setState({board:board,hasWon:hasWon});
   }
 
-   canI(){
-    for(let i=-0;i<6;i++){
-        <tr key={i}>
-           canI2();
-        </tr>
-    }
-   }
-   canI2(){
-    for(let j=0;j<6;j++){
-        <Cell key={j}/>
-    }
-   }
-
-  /** Render game board or winning message. */
+  /** Renders game board or winning message. */
 
   render() {
+    if(this.state.hasWon){
+      return <h1 className="neon-green">You Won!</h1>
+    }
+    let tblBoard=[];
+    for(let y=0;y<this.props.nrows;y++){
+      let row=[];
+      for(let x=0;x<this.props.ncols;x++){
+        let coord=`${y}-${x}`;
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]}
+        flipCellsAroundMe={()=>this.flipCellsAround(coord)} />) 
+      }
+      tblBoard.push(<tr key={y}>{row }</tr>)
+    }
 
-    // if the game is won, just show a winning msg & render nothing else
-
-    // TODO
-
-    // make table board
-
-    // TODO
     return(
-        <div >
+        <div>
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
             <table className="Board">
                 <tbody>
-                    <tr><Cell isLit={true}/>
-                    <Cell isLit={false}/>
-                    <Cell isLit={true}/>
-                    
-                    </tr>
+                  {tblBoard}
                 </tbody>
             </table>
         </div>
