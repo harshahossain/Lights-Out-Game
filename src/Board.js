f9674f97acd228cad1cb8070f5074ac0ab0ba30e
Harsha Hossain <harshahossain@gmail.com>
2023-09-15 11:44:23 +0600

    Tweaked notes, unnecessary codes.
    Cleaned up unnecessary codes, tweaked up notes.

--- src/Board.js
+++ src/Board.js
@@ -2,35 +2,6 @@ import React, {Component} from "react";
 import Cell from "./Cell";
 import './Board.css';
 
-
-
-
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
 class Board extends Component {
 static defaultProps={
   nrows:5,
@@ -39,19 +10,18 @@ static defaultProps={
 };
   constructor(props) {
     super(props);
-    
-    // TODO: set initial state
+    //Seting initial game-board state
     this.state={
       hasWon:false,
       board:this.createBoard()
     };
   }
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+  // Creating a board nrows high/ncols wide, each cell randomly lit or unlit 
 
   createBoard() {
     let board = [];
-    // TODO: create array-of-arrays of true/false values
+    //Creating array-of-arrays of true/false values
      for(let y=0;y<this.props.nrows;y++){
       let row=[];
       for(let x=0; x<this.props.ncols;x++){
@@ -62,7 +32,7 @@ static defaultProps={
     return board
   }
 
-  /** handle changing a cell: update board & determine if winner */
+//  Updating board and checking if the winning conditions are met
 
   flipCellsAround(coord) {
     console.log('flipping')
@@ -80,17 +50,14 @@ static defaultProps={
       }
     }
     flipCell(y,x);//updating the clicked cell
-    //updating ne-ighbourr cell;down below
+    //updating ne-ighbourr cell;
     flipCell(y,x-1);//flip left
     flipCell(y,x+1);//flip right
     flipCell(y-1,x);//flip down
     flipCell(y+1,x);//flip up
-    // let hasWon= this.state.hasWon;
     let hasWon=board.every(row=>row.every(cell=>!cell))//just means every rows every cell should be false
     this.setState({board:board,hasWon:hasWon});
   }
-
-  /** Renders game board or winning message. */
 
   render() {
     if(this.state.hasWon){
